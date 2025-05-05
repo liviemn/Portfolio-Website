6200457f3f58114279d7c1df0090af9e6e981173
liviemn <liviemn@gmail.com>
2025-05-04 20:17:17 -0400

    Boxes Commit

--- src/Components/AboutBoxes.tsx
+++ src/Components/AboutBoxes.tsx
@@ -8,11 +8,11 @@ const AboutBoxes = () => {
                     <div className="flex space-x-4">
 
                         {/* INTERESTS Box */}
-                        <div className="relative w-48 lg:w-48 xl:w-56 h-53 lg:h-53 xl:h-60 bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 lg:p-4 xl:p-6 flex border-2 border-[#3E2B2B]">
-                            <div className="absolute top-[-20px] left-3 w-25 lg:w-25 xl:w-28 h-10 lg:h-10 xl:h-12 bg-[#3E2B2B] rounded-full flex items-center justify-center">
-                                <span className="text-sm lg:text-sm xl:text-base text-white font-poetsen">INTERESTS</span>
+                        <div className="relative w-48 xl:w-48 2xl:w-56 h-53 xl:h-53 2xl:h-60 bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 xl:p-4 2xl:p-6 flex border-2 border-[#3E2B2B]">
+                            <div className="absolute top-[-20px] left-3 w-25 xl:w-25 2xl:w-28 h-10 xl:h-10 2xl:h-12 bg-[#3E2B2B] rounded-full flex items-center justify-center">
+                                <span className="text-sm xl:text-sm 2xl:text-base text-white font-poetsen">INTERESTS</span>
                             </div>
-                            <ul className="list-disc pl-6 pt-4 text-sm lg:text-sm xl:text-base space-y-1">
+                            <ul className="list-disc pl-6 pt-4 text-sm xl:text-sm 2xl:text-base space-y-1">
                                 <li>MBTI</li>
                                 <li>Gaming</li>
                                 <li>Baking</li>
@@ -23,11 +23,11 @@ const AboutBoxes = () => {
                         </div>
 
                         {/* GAMES Box */}
-                        <div className="relative w-48 lg:w-48 xl:w-56 h-53 lg:h-53 xl:h-60 bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 lg:p-4 xl:p-6 flex border-2 border-[#3E2B2B]">
-                            <div className="absolute top-[-20px] left-3 w-25 lg:w-25 xl:w-28 h-10 lg:h-10 xl:h-12 bg-[#3E2B2B] rounded-full flex items-center justify-center">
-                                <span className="text-sm lg:text-sm xl:text-base text-white font-poetsen">GAMES</span>
+                        <div className="relative w-48 xl:w-48 2xl:w-56 h-53 xl:h-53 2xl:h-60 bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 xl:p-4 2xl:p-6 flex border-2 border-[#3E2B2B]">
+                            <div className="absolute top-[-20px] left-3 w-25 xl:w-25 2xl:w-28 h-10 xl:h-10 2xl:h-12 bg-[#3E2B2B] rounded-full flex items-center justify-center">
+                                <span className="text-sm xl:text-sm 2xl:text-base text-white font-poetsen">GAMES</span>
                             </div>
-                            <ul className="list-disc pl-6 pt-4 text-sm lg:text-sm xl:text-base space-y-1">
+                            <ul className="list-disc pl-6 pt-4 text-sm xl:text-sm 2xl:text-base space-y-1">
                                 <li>Animal Crossing</li>
                                 <li>Valorant</li>
                                 <li>Minecraft</li>
@@ -39,11 +39,11 @@ const AboutBoxes = () => {
                     </div>
 
                     {/* HOBBIES Box */}
-                    <div className="relative w-100 lg:w-100 xl:w-[28rem] h-38 lg:h-38 xl:h-44 bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 lg:p-4 xl:p-6 flex justify-center border-2 border-[#3E2B2B]">
-                        <div className="absolute top-[-20px] left-3 w-25 lg:w-25 xl:w-28 h-10 lg:h-10 xl:h-12 bg-[#3E2B2B] rounded-full flex items-center justify-center">
-                            <span className="text-sm lg:text-sm xl:text-base text-white font-poetsen">HOBBIES</span>
+                    <div className="relative w-100 xl:w-100 2xl:w-[28rem] h-38 xl:h-38 2xl:h-44 bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 xl:p-4 2xl:p-6 flex justify-center border-2 border-[#3E2B2B]">
+                        <div className="absolute top-[-20px] left-3 w-25 xl:w-25 2xl:w-28 h-10 xl:h-10 2xl:h-12 bg-[#3E2B2B] rounded-full flex items-center justify-center">
+                            <span className="text-sm xl:text-sm 2xl:text-base text-white font-poetsen">HOBBIES</span>
                         </div>
-                        <div className="pt-4 grid grid-cols-2 gap-x-8 text-sm lg:text-sm xl:text-base">
+                        <div className="pt-4 grid grid-cols-2 gap-x-8 text-sm xl:text-sm 2xl:text-base">
                             <ul className="list-disc pl-5 space-y-1">
                                 <li>Shopping</li>
                                 <li>Studying at Cafes</li>
