70d2bf29a0e990ba6b4a8ebda8dd8383a5d95654
liviemn <liviemn@gmail.com>
2025-05-05 00:48:35 -0400

    .

--- src/Components/Introduction.tsx
+++ src/Components/Introduction.tsx
@@ -3,8 +3,8 @@ import { Typewriter } from 'react-simple-typewriter';
 
 const Introduction: React.FC = () => {
   return (
-    <div className="flex flex-col justify-center h-full py-16 xl:py-16 2xl:py-20">
-      <div className="max-w-3xl font-poetsen text-[#3E2B2B] text-left text-6xl xl:text-6xl 2xl:text-7xl ml-4 xl:ml-4 2xl:ml-6">
+    <div className="flex flex-col justify-center h-[35vh] py-[8vh]">
+      <div className="max-w-[45vw] font-poetsen text-[#3E2B2B] text-left text-[4.2vw] ml-[1vw]">
         <Typewriter
           words={["Hello!", "My name is Olivia :)"]}
           loop
@@ -16,7 +16,7 @@ const Introduction: React.FC = () => {
         />
       </div>
 
-      <p className="text-base xl:text-base 2xl:text-lg font-poetsen !text-[#3E2B2B] mt-8 mb-6 max-w-md xl:max-w-md 2xl:max-w-lg ml-4 xl:ml-4 2xl:ml-6 leading-8 xl:leading-8 2xl:leading-9">
+      <p className="text-base xl:text-base 2xl:text-lg font-poetsen !text-[#3E2B2B] mt-[2vh] mb-[3vh] max-w-[33vw] ml-[1vw] leading-[2.3vw]">
         I'm a computer science major at George Mason University. Years of experience in customer service
         and proficient in Java, Python, HTML & CSS, and C. Actively looking for internships or
         other opportunities!
