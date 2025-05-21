c684c606a28583704b6ea2367f23b1734fe15cd5
liviemn <liviemn@gmail.com>
2025-05-21 15:45:41 -0400

    .

--- src/Components/AboutBoxes.tsx
+++ src/Components/AboutBoxes.tsx
@@ -2,17 +2,17 @@ import FadeIn from '../Components/FadeIn'
 
 const AboutBoxes = () => {
     return (
-        <div className="absolute left-[30%] top-[33.5%] transform -translate-x-1/2">
+        <div className="absolute left-[30%] top-[33.5%] transform -translate-x-1/2 max-w-[90vw] px-4">
             <FadeIn>
-                <div className="flex flex-col space-y-[4vh]">
-                    <div className="flex space-x-[1.5vw]">
+                <div className="flex flex-col space-y-8 sm:space-y-10">
+                    <div className="flex flex-col sm:flex-row sm:space-x-6 space-y-6 sm:space-y-0">
 
                         {/* INTERESTS Box */}
-                        <div className="relative w-[14vw] h-[26vh] bg-white text-[#3E2B2B] rounded-[1vw] shadow-md p-[1vw] flex border-[0.2vw] border-[#3E2B2B]">
-                            <div className="absolute top-[-2.5vh] left-[1vw] w-[7vw] h-[5vh] bg-[#3E2B2B] rounded-full flex items-center justify-center">
-                                <span className="text-[1vw] text-white font-poetsen">INTERESTS</span>
+                        <div className="relative w-full sm:w-1/2 max-w-[400px] bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 border-3 border-[#3E2B2B]">
+                            <div className="absolute -top-5 left-4 w-[50%] h-[5vh] bg-[#3E2B2B] rounded-full flex items-center justify-center">
+                                <span className="text-sm sm:text-base text-white font-poetsen">INTERESTS</span>
                             </div>
-                            <ul className="list-disc pl-[2vw] pt-[2vh] text-[1vw] space-y-[0.2vh]">
+                            <ul className="list-disc pl-6 pt-4.5 text-xs sm:text-sm space-y-1.5">
                                 <li>MBTI</li>
                                 <li>Gaming</li>
                                 <li>Baking</li>
@@ -23,11 +23,11 @@ const AboutBoxes = () => {
                         </div>
 
                         {/* GAMES Box */}
-                        <div className="relative w-[14vw] h-[26vh] bg-white text-[#3E2B2B] rounded-[1vw] shadow-md p-[1vw] flex border-[0.2vw] border-[#3E2B2B]">
-                            <div className="absolute top-[-2.5vh] left-[1vw] w-[7vw] h-[5vh] bg-[#3E2B2B] rounded-full flex items-center justify-center">
-                                <span className="text-[1vw] text-white font-poetsen">GAMES</span>
+                        <div className="relative w-full sm:w-1/2 max-w-[400px] bg-white text-[#3E2B2B] rounded-xl shadow-md p-4 border-3 border-[#3E2B2B]">
+                            <div className="absolute -top-5 left-4 w-[50%] h-[5vh] bg-[#3E2B2B] rounded-full flex items-center justify-center">
+                                <span className="text-sm sm:text-base text-white font-poetsen">GAMES</span>
                             </div>
-                            <ul className="list-disc pl-[2vw] pt-[2vh] text-[1vw] space-y-[0.2vh]">
+                            <ul className="list-disc pl-6 pt-4.5 text-xs sm:text-sm space-y-1.5">
                                 <li>Animal Crossing</li>
                                 <li>Valorant</li>
                                 <li>Minecraft</li>
