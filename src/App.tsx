2ccaaf73c61bae7b2130a7a5379026ba91135c23
liviemn <liviemn@gmail.com>
2025-05-29 20:15:20 -0400

    Design Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import Skills from './Pages/Skills';
 import Projects from './Pages/Projects'
 
 function App() {
-  return (
-    <div>
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-    </div>
-  );
-}
+    return (
+      <div>
+        <div className="section-home">
+          <Home />
+        </div>
+        <div className="section-about">
+          <About />
+        </div>
+        <div className="section-skills">
+          <Skills />
+        </div>
+        <div className="section-projects">
+          <Projects />
+        </div>
+      </div>
+    );
+  }
+  
 
 export default App;
