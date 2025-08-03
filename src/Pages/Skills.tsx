f0fbc64e8fc7221c6139291667d06fc3b7f28420
liviemn <liviemn@gmail.com>
2025-08-03 02:02:40 -0400

    Skills Commit

--- src/Pages/Skills.tsx
+++ src/Pages/Skills.tsx
@@ -17,7 +17,7 @@ function Skills() {
       <div
         className="text-[#3E2B2B] text-[8vw] sm:text-[4.5vw] font-poetsen font-[800] z-20 
                   mt-[7vh] sm:mt-[-5vh] 
-                  sm:absolute sm:top-[15vh] sm:right-[15vw]"
+                  sm:absolute sm:top-[15vh] sm:right-[5vw]"
       >
         {isMobile ? (
           <motion.div
