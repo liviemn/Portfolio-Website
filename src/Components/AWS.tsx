6498543da235cd9f814aa5d60d3f5f89ab62be27
liviemn <liviemn@gmail.com>
2025-08-04 13:14:09 -0400

    .

--- src/Components/AWS.tsx
+++ src/Components/AWS.tsx
@@ -25,7 +25,7 @@ const AWS: React.FC = () => {
       ref={ref}
       initial={{ opacity: 0, y: 50 }}
       animate={controls}
-      className="w-full flex justify-center mt-86 sm:mt-120 xlg:mt-140"
+      className="w-full flex justify-center mt-86 sm:mt-[58vh] xlg:mt-[78vh]"
     >
       <div
         onClick={() => setFlipped(!flipped)}
